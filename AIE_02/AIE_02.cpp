238f44ce69cdf7b408f1956d55a7bff0d4ee7a7f
basechase <chase7y@gmail.com>
2024-12-06 16:34:22 -0600

    completed array exercise aie_02

--- AIE_02/AIE_02.cpp
+++ AIE_02/AIE_02.cpp
@@ -30,18 +30,46 @@ int main(int argc, char** argv)
 int MinValue(const int* arr, int count)
 {
 	// TODO: loop through the array, return the smallest value
-	return 0;
+	int min = arr[0];
+	for (int i = 1; i < count; i++)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+
+	return min;
 }
 
 int MaxValue(const int* arr, int count)
 {
 	// TODO: loop through the array, return the largest value
-	return 0;
+	int max = arr[0];
+	for (int i = 1; i < count; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+
+
+
+	return max;
 }
 
 int CountOccurrencesOfValue(const int* arr, int count, int search)
 {
+	int countSearch = 0;
 	// TODO: loop through the array, return the number of times the search value occurs 
-	return 0;
+	for (int i = 0; i < count; i++)
+	{
+		if (arr[i] == search) 
+		{
+			countSearch++;
+		}
+	}
+	return countSearch;
 }
 
