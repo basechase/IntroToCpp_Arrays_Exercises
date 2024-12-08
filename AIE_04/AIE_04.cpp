d197c09bd0aecd5233f8244875fcafff0743ae02
basechase <chase7y@gmail.com>
2024-12-07 18:53:22 -0600

    started array sort

--- AIE_04/AIE_04.cpp
+++ AIE_04/AIE_04.cpp
@@ -37,4 +37,15 @@ void SortArray(int* arr, int count)
 	// TODO: 
 	// modify the values in the array so that when this method returns
 	// all values are sorted in ascending order
+
+	for (int i = 0; i < arr[i + 1]; i++)
+	{
+
+	
+
+	}
+
+
+
+
 }
